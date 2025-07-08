ec4d7056083deab77484bebf07d7030f84c88226
LanceStemple <stemplel@msu.edu>
2025-07-08 19:27:02 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LancesApp',
+  webDir: 'www'
+};
+
+export default config;
